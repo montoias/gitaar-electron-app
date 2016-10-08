350209c0e16138c71d0d9ef801e4bdd4beabfd9b
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2016-10-08 01:10:52 +0100

    Add spinner on uploading to github

--- app/components/edit-song-page.js
+++ app/components/edit-song-page.js
@@ -3,13 +3,16 @@ const ipc = require('electron').ipcRenderer;
 
 export default Ember.Component.extend({
   name: null,
+  isCommiting: false,
 
   actions: {
     commitAndPush() {
+      this.set('isCommiting', true);
       ipc.send('git-commit-push', this.get('name'));
 
       ipc.on('git-commit-push', (event, isValid) => {
         console.log(isValid);
+        this.set('isCommiting', false);
       });
     }
   }
